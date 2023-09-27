3d24c8df1e4308b35d5aa6e72ee554b170552da4
likhitha1519 <145051890+likhitha1519@users.noreply.github.com>
2023-09-27 19:37:36 -0400

    Initial commit

---
+++ Printer.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+class Printer
+{
+  public static < V > void printArray( V[] inputArray )
+   {
+      // Display array elements              
+         for ( V element : inputArray ){        
+            System.out.println(element);
+         }
+    }
+  
+}  
+ 
+
+public class Solution {
+
+
+    public static void main( String args[] ) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = { 1, 2, 3 };
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+
+            if(name.equals("printArray"))
+                count++;
+        }
+
+        if(count > 1)System.out.println("Method overloading is not allowed!");
+      
+    }
+}
